a6cd30082de1e64ce06c333e1891685c48dc5fa6
Parth Shah <parth@linux.ibm.com>
2019-10-07 14:00:49 +0530

    sched/fair: Tune task wake-up logic to pack small background tasks on fewer cores
    The algorithm finds the first non idle core in the system and tries to
    place a task in the idle CPU in the chosen core. To maintain
    cache hotness, work of finding non idle core starts from the prev_cpu,
    which also reduces task ping-pong behaviour inside of the core.
    
    Define a new method to select_non_idle_core which keep tracks of the idle
    and non-idle CPUs in the core and based on the heuristics determines if the
    core is sufficiently busy to place the incoming backgroung task. The
    heuristic further defines the non-idle CPU into either busy (>12.5% util)
    CPU and overutilized (>80% util) CPU.
    - The core containing more idle CPUs and no busy CPUs is not selected for
      packing
    - The core if contains more than 1 overutilized CPUs are exempted from
      task packing
    - Pack if there is atleast one busy CPU and overutilized CPUs count is <2
    
    Value of 12.5% utilization for busy CPU gives sufficient heuristics for CPU
    doing enough work and not become idle in nearby timeframe.
    
    Signed-off-by: Parth Shah <parth@linux.ibm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6610,6 +6610,7 @@ static struct kmem_cache *task_group_cache __read_mostly;
 
 DECLARE_PER_CPU(cpumask_var_t, load_balance_mask);
 DECLARE_PER_CPU(cpumask_var_t, select_idle_mask);
+DECLARE_PER_CPU(cpumask_var_t, turbo_sched_mask);
 
 void __init sched_init(void)
 {
@@ -6649,6 +6650,8 @@ void __init sched_init(void)
 		per_cpu(load_balance_mask, i) = (cpumask_var_t)kzalloc_node(
 			cpumask_size(), GFP_KERNEL, cpu_to_node(i));
 		per_cpu(select_idle_mask, i) = (cpumask_var_t)kzalloc_node(
+			cpumask_size(), GFP_KERNEL, cpu_to_node(i));
+		per_cpu(turbo_sched_mask, i) = (cpumask_var_t)kzalloc_node(
 			cpumask_size(), GFP_KERNEL, cpu_to_node(i));
 	}
 #endif /* CONFIG_CPUMASK_OFFSTACK */
