e8626393ca3cde0fb4ba70ca0417bb17166c3e6a
Parth Shah <parth@linux.ibm.com>
2020-01-09 18:41:07 +0530

    Refresh of sched-core-add-manual

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2499,3 +2499,5 @@ static inline void membarrier_switch_mm(struct rq *rq,
 #endif
 
 #define lenient_latency(lat)	((lat) == MAX_LATENCY_TOLERANCE)
+#define task_latency_lenient(lat)	\
+	(lenient_latency((p)->latency_tolerance))
