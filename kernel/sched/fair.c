3982dc60e1c5d338eb7c90fad40cfbff2a78785a
Parth Shah <parth@linux.ibm.com>
2020-09-22 14:47:16 +0530

    sched/fair: Use available_idle_cpu abstraction everywhere

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5832,7 +5832,7 @@ wake_affine_idle(int this_cpu, int prev_cpu, int sync)
 	 * on one CPU.
 	 */
 	if (available_idle_cpu(this_cpu) && cpus_share_cache(this_cpu, prev_cpu))
-		return available_idle_cpu(prev_cpu) ? prev_cpu : this_cpu;
+		return is_idle_cpu(prev_cpu) ? prev_cpu : this_cpu;
 
 	if (sync && cpu_rq(this_cpu)->nr_running == 1)
 		return this_cpu;
