fedf2339a58fb60ed908bf569d2f987e4a4c3351
Parth Shah <parth@linux.ibm.com>
2020-09-23 10:18:18 +0530

    sched/fair: Fix nr accounting in select_idle_cpu
    select_idle_cpu() iterates through maximum of nr cpus to find if a CPU is
    idle or not. The value of nr is set based on the average scan cost and
    average idle time of the current CPU, but it is lower bounded by 4 CPUs.
    Hence atleast 4 CPUs will need to be searched in the case when current CPU
    is too busy.
    
    However, due to pre-decrement check of nr makes search for only 3 CPUs in
    most busiest case. Hence fix this by changing this to post-decrement.
    
    Signed-off-by: Parth Shah <parth@linux.ibm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6147,7 +6147,7 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 	cpumask_and(cpus, sched_domain_span(sd), p->cpus_ptr);
 
 	for_each_cpu_wrap(cpu, cpus, target) {
-		if (!--nr)
+		if (!nr--)
 			return -1;
 		if (available_idle_cpu(cpu) || sched_idle_cpu(cpu))
 			break;
