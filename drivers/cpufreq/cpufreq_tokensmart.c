e0120f857d35530a16711a9d6a80668d3666e528
Parth Shah <parth@linux.ibm.com>
2021-06-16 11:28:33 +0530

    Refresh of temp

--- drivers/cpufreq/cpufreq_tokensmart.c
+++ drivers/cpufreq/cpufreq_tokensmart.c
@@ -14,8 +14,6 @@
 #include <linux/slab.h>
 #include <linux/tick.h>
 #include <linux/sched/cpufreq.h>
-#include <linux/percpu-defs.h>
-#include "cpufreq_governor.h"
 
 #define CPUS_PER_QUAD 16
 #define CPUS_PER_POLICY	4
