6d5098eb5b164ba3ca284c5ae0ed8c814e9cc590
Parth Shah <parth@linux.ibm.com>
2021-06-21 15:28:42 +0530

    host work

--- arch/powerpc/kvm/book3s.h
+++ arch/powerpc/kvm/book3s.h
@@ -27,6 +27,9 @@ extern void kvmppc_book3s_exit_pr(void);
 
 extern void kvmppc_idle_hint_set(struct kvm_vcpu *vcpu, int idle_hint);
 
+DECLARE_PER_CPU(struct list_head, idle_hint_subscribers);
+DECLARE_PER_CPU(spinlock_t, idle_hint_subscribers_lock);
+
 #ifdef CONFIG_PPC_TRANSACTIONAL_MEM
 extern void kvmppc_emulate_tabort(struct kvm_vcpu *vcpu, int ra_val);
 #else
