21bb4576b35bf9b54e6b6ada8725cd9fcd7b4700
Parth Shah <parth@linux.ibm.com>
2021-06-21 16:31:05 +0530

    set/clear idle hint in do_idle loop

---
+++ arch/powerpc/include/asm/idle_hint.h
@@ -0,0 +1,11 @@
+#ifndef _ASM_POWERPC_IDLEHINT_H
+#define _ASM_POWERPC_IDLEHINT_H
+
+#include <linux/kvm_host.h>
+
+extern void kvmppc_idle_hint_set(struct kvm_vcpu *vcpu, int idle_hint);
+
+DECLARE_PER_CPU(struct list_head, idle_hint_subscribers);
+DECLARE_PER_CPU(spinlock_t, idle_hint_subscribers_lock);
+
+#endif
