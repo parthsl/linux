ebad0fba4fa0e6f56b733c6721c1417b7cda2fdc
Parth Shah <parth@linux.ibm.com>
2021-06-22 17:14:02 +0530

    Refresh of tokensmart-add-a-new-cpufreq

--- drivers/cpufreq/cpufreq_tokensmart.c
+++ drivers/cpufreq/cpufreq_tokensmart.c
@@ -231,7 +231,7 @@ static void tg_exit(struct dbs_data *dbs_data)
 
 static void tg_start(struct cpufreq_policy *policy)
 {
-	int i;
+	int cpu;
 
 	topology.nr_policies = 0;
 
@@ -242,8 +242,8 @@ static void tg_start(struct cpufreq_policy *policy)
 		tg_data = kzalloc(sizeof(struct tgdbs)*topology.nr_policies, GFP_KERNEL);
 
 		avg_load_per_quad = kzalloc(sizeof(struct avg_load_per_quad)*topology.nr_cpus, GFP_KERNEL);
-		for(i = 0; i<topology.nr_cpus; i++)
-			avg_load_per_quad[i].load = (unsigned int*) kmalloc(sizeof(unsigned int) * policies_per_fd, GFP_KERNEL);
+		for(cpu = 0; i<topology.nr_cpus; cpu++)
+			avg_load_per_quad[cpu].load = (unsigned int*) kmalloc(sizeof(unsigned int) * policies_per_fd, GFP_KERNEL);
 
 		pool_turn = 0;
 
