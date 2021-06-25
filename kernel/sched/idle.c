0dc70dbb7b4560929d6f94af968320979fbf7bcd
Parth Shah <parth@linux.ibm.com>
2021-06-25 10:49:14 +0530

    Refresh of set-clear-idle-hint-in-do_idle

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -258,12 +258,12 @@ static void flag_idle_hint(int cpu, int flag)
 {
 	struct kvm_vcpu *pos;
 
-	spin_lock(&per_cpu(idle_hint_subscribers_lock, cpu);)
+	spin_lock(&per_cpu(idle_hint_subscribers_lock, cpu));
 	list_for_each_entry(pos, &per_cpu(idle_hint_subscribers, cpu),
 			    idle_hint_subscribers) {
 		kvmppc_idle_hint_set(pos, flag);
 	}
-	spin_unlock(&per_cpu(idle_hint_subscribers_lock, cpu);)
+	spin_unlock(&per_cpu(idle_hint_subscribers_lock, cpu));
 }
 
 /*
