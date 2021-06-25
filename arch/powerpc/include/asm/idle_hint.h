064f15e9e6fb609584cb576e5f6d937bc0555c8d
Parth Shah <parth@linux.ibm.com>
2021-06-25 10:48:54 +0530

    Refresh of set-clear-idle-hint-in-do_idle

--- arch/powerpc/include/asm/idle_hint.h
+++ arch/powerpc/include/asm/idle_hint.h
@@ -1,6 +1,8 @@
 #ifndef _ASM_POWERPC_IDLEHINT_H
 #define _ASM_POWERPC_IDLEHINT_H
 
+#include <linux/kvm_host.h>
+
 extern void kvmppc_idle_hint_set(struct kvm_vcpu *vcpu, int idle_hint);
 
 DECLARE_PER_CPU(struct list_head, idle_hint_subscribers);
