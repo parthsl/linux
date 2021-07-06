1eae65715c884731e7d5588281619a3b78adfd9e
Parth Shah <parth@linux.ibm.com>
2021-07-06 12:44:17 +0530

    guest work
    host work
    set/clear idle hint in do_idle loop
    debug bits
    walk through all vcpus of all kvms and give idle hint
    remove spinlocks temp
    irough
    to be merged
    small fix

---
+++ arch/powerpc/include/asm/idle_hint.h
@@ -0,0 +1,32 @@
+#ifndef _ASM_POWERPC_IDLEHINT_H
+#define _ASM_POWERPC_IDLEHINT_H
+
+#include <linux/kvm_host.h>
+
+extern void kvmppc_idle_hint_set(struct kvm_vcpu *vcpu, int idle_hint);
+
+extern int idle_hint_is_active;
+
+extern void flagit(int cpu, int flag);
+
+static inline int prev_cpu_of_kvm(struct kvm_vcpu *vcpu)
+{
+	struct pid *pid;
+	struct task_struct *task = NULL;
+
+	rcu_read_lock();
+	pid = rcu_dereference(vcpu->pid);
+	if (pid)
+		task = get_pid_task(pid, PIDTYPE_PID);
+	rcu_read_unlock();
+
+	if (!task)
+		return -1;
+
+	return task_cpu(task);
+}
+
+DECLARE_PER_CPU(struct list_head, idle_hint_subscribers);
+DECLARE_PER_CPU(spinlock_t, idle_hint_subscribers_lock);
+
+#endif
